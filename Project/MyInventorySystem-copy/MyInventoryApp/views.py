7b3b2db36bffa5bb25f33110edf682ad455b749d
miguelgeoffreycootauco-oss <miguel.geoffrey.cootauco@student.ateneo.edu>
2026-04-13 14:07:47 +0800

    done

--- Project/MyInventorySystem-copy/MyInventoryApp/views.py
+++ Project/MyInventorySystem-copy/MyInventoryApp/views.py
@@ -5,11 +5,17 @@ from .models import Supplier, WaterBottle, Account
 
 def view_supplier(request):
     suppliers = Supplier.objects.all()
-    return render(request, 'MyInventoryApp/supplier.html', {'suppliers': suppliers})
+    return render(request, 'MyInventoryApp/supplier.html', {
+        'suppliers': suppliers,
+        'current_user_id': request.session.get('user_id')
+    })
 
 def view_bottles(request):
     bottles = WaterBottle.objects.all()
-    return render(request, 'MyInventoryApp/bottles.html', {'bottles': bottles})
+    return render(request, 'MyInventoryApp/bottles.html', {
+        'bottles': bottles,
+        'current_user_id': request.session.get('user_id')
+    })
 
 def add_bottle(request):
     if request.method == 'POST':
@@ -33,13 +39,47 @@ def add_bottle(request):
             supplied_by=supplier_obj
             )
         return redirect('view_bottles')
-    
     all_suppliers = Supplier.objects.all()
-    return render(request, 'MyInventoryApp/add_bottle.html', {'suppliers': all_suppliers})
-
+    return render(request, 'MyInventoryApp/add_bottle.html', {
+        'suppliers': all_suppliers,
+        'current_user_id': request.session.get('user_id') # ADD THIS
+    })
+    
 def manage_account(request, pk):
+    if not request.session.get('user_id'):
+        return redirect('login')
+        
     account_obj = get_object_or_404(Account, pk=pk)
-    return render(request, 'MyInventoryApp/manage_account.html', {'account': account_obj})
+    return render(request, 'MyInventoryApp/manage_account.html', {
+        'account': account_obj,
+        'current_user_id': request.session.get('user_id')
+    })
+
+def change_password(request, pk):
+    account_obj = get_object_or_404(Account, pk=pk)
+    error = None
+
+    if request.method == 'POST':
+        current_p = request.POST.get('current_password')
+        new_p = request.POST.get('new_password')
+        confirm_p = request.POST.get('confirm_password')
+
+        if account_obj.password != current_p:
+            error = "Current password is incorrect."
+        
+        elif new_p != confirm_p:
+            error = "New passwords do not match."
+            
+        else:
+            account_obj.password = new_p
+            account_obj.save()
+            return redirect('manage_account', pk=pk)
+    
+    return render(request, 'MyInventoryApp/change_password.html', {
+        'account': account_obj,
+        'error': error,
+        'current_user_id': request.session.get('user_id')
+    })
 
 def delete_account(request, pk):
     account_obj = get_object_or_404(Account, pk=pk)
@@ -49,25 +89,18 @@ def delete_account(request, pk):
         
     return redirect('login')
 
-def change_password(request, pk):
-    account_obj = get_object_or_404(Account, pk=pk)
-    if request.method == 'POST':
-        new_password = request.POST.get('new_password')
-        account_obj.password = new_password
-        account_obj.save()
-        return redirect('manage_account', pk=pk)
-    
-    return render(request, 'MyInventoryApp/change_password.html', {'account': account_obj})
-
 def login_view(request): 
     if request.method == "POST": 
-        username = request.POST.get('username') 
-        password = request.POST.get('password') 
-        account = Account.objects.filter(username = username, password = password)
-        if account: 
-            return redirect('view_supplier') 
+        u = request.POST.get('username') 
+        p = request.POST.get('password') 
+        account_results = Account.objects.filter(username=u, password=p)
+        
+        if account_results.exists(): 
+            user_obj = account_results.first()
+            request.session['user_id'] = user_obj.id 
+            return redirect('view_bottles') 
         else: 
-            return render(request, 'MyInventoryApp/login.html', {'error_message': 'Invalid login'})
+            return render(request, 'MyInventoryApp/login.html', {'error': 'Invalid login'})
     return render(request, 'MyInventoryApp/login.html')
 
 def signup(request): 
@@ -82,4 +115,7 @@ def signup(request):
             return render(request, 'MyInventoryApp/login.html', {'success_message': 'Account created successfully'}) 
     return render(request, 'MyInventoryApp/signup.html')
 
-    
+def index(request): # Or whatever your root view is named
+    return render(request, 'MyInventoryApp/index.html', {
+        'current_user_id': request.session.get('user_id')
+    })
